d8e3a4a069b756370c667a6db82295896b913415
sigrung15 <sigrung15@ru.is>
2016-10-10 10:23:04 +0000

    Implemented the Add function, tested the Empty string and it worked

---
+++ StringCalculator.java
@@ -0,0 +1,12 @@
+public class StringCalculator {
+	public static int Add(String n){
+		if(n.isEmpty())
+			return 0;
+		return 0;
+	}
+	
+	public static void main(String[] args){
+		String empty = "";
+		System.out.println("This should print out 0: " + Add(empty));
+	}
+}
