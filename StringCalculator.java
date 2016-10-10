92232b5077f058c2033757673930fa0e725105fd
sigrung15 <sigrung15@ru.is>
2016-10-10 10:31:01 +0000

    Implemented in the add function if I have one number, Tested - it did only work for one numbers from 0-9 but not larger

--- StringCalculator.java
+++ StringCalculator.java
@@ -2,11 +2,17 @@ public class StringCalculator {
 	public static int Add(String n){
 		if(n.isEmpty())
 			return 0;
+		else if(n.length() == 1)
+			return Integer.valueOf(n);
 		return 0;
 	}
 	
 	public static void main(String[] args){
 		String empty = "";
 		System.out.println("This should print out 0: " + Add(empty));
+		String oneNr = "1";
+		System.out.println("This should print out one number: " + Add(oneNr));
+		oneNr = "72";
+		System.out.println("This should print out one number: " + Add(oneNr));
 	}
 }
