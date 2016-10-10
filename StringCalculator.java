0f31eb0e390a8e2939bf534474c4f1da633e2e74
sigrung15 <sigrung15@ru.is>
2016-10-10 10:41:46 +0000

    Changed Add function so that it can take in two numbers separated by comma and returnes the sum, Tested and it returned the sum between the two no matter how large the number.

--- StringCalculator.java
+++ StringCalculator.java
@@ -5,7 +5,13 @@ public class StringCalculator {
 		String[] numbers = n.split(",");
 		if(numbers.length == 1)
 			return Integer.valueOf(numbers[0]);
-		return 0;
+		else{
+			int sum = 0;
+			for(int i = 0; i < numbers.length; i++){
+				sum += Integer.valueOf(numbers[i]);
+			}
+			return sum;
+		}
 	}
 	
 	public static void main(String[] args){
@@ -15,5 +21,9 @@ public class StringCalculator {
 		System.out.println("This should print out one number: " + Add(oneNr));
 		oneNr = "72";
 		System.out.println("This should print out one number: " + Add(oneNr));
+		String twoNr = "12,14";
+		System.out.println("This should print out one number: " + Add(twoNr));
+		twoNr = "122,309";
+		System.out.println("This should print out one number: " + Add(twoNr));
 	}
 }
