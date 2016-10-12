7c05bccf5d7676b28e81f1f44e79fd14d186b303
sigrung15 <sigrung15@ru.is>
2016-10-12 10:20:01 +0000

    test two numbers passed

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -12,4 +12,8 @@ public class CalculatorTest{
   public void testOneNumber(){
     assertEquals(1, Calculator.add("1"));
   }
+  @Test
+  public void testTwoNumbers(){
+    assertEquals(3, Calculator.add("1,2"));
+  }
 }
